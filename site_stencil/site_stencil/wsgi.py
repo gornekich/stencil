4c0949bb4477de303571cd293bfd302127169bbe
nikigor <n.gorbadey@gmail.com>
2019-01-04 21:33:09 +0300

    Introduce site for stencil application
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

---
+++ site_stencil/site_stencil/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for site_stencil project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_stencil.settings')
+
+application = get_wsgi_application()
