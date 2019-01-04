1bd7d9523d2967373ca8b8a095093fb168807c2d
Valery Statinov <valery.statinov@gmail.com>
2019-01-04 22:06:25 +0300

    Introduce frontend
    Signed-off-by: Valery Statinov <valery.statinov@gmail.com>

---
+++ site_stencil/frontend/build/precache-manifest.9ebf9c715a3e566c475ece506ac677ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "26140a8319d0fb385bd5",
+    "url": "/static/js/main.26140a83.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "26140a8319d0fb385bd5",
+    "url": "/static/css/main.4dad88aa.chunk.css"
+  },
+  {
+    "revision": "50df50ca5d14f7fab79c37e998073af3",
+    "url": "/index.html"
+  }
+];
---
+++ site_stencil/frontend/build/precache-manifest.f109cb71bc4fa7c477173a6ee664ac11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06f1f6236e6351653ac4",
+    "url": "/static/js/main.06f1f623.chunk.js"
+  },
+  {
+    "revision": "75e4f3974ab2dd1b8c19",
+    "url": "/static/js/1.75e4f397.chunk.js"
+  },
+  {
+    "revision": "06f1f6236e6351653ac4",
+    "url": "/static/css/main.165fbda3.chunk.css"
+  },
+  {
+    "revision": "106ad44505b9ed6f1d712ed01a129771",
+    "url": "/index.html"
+  }
+];
