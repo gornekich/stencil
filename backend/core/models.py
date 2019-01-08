d96bf0a2a0f2888348c4c2dab15beeea535373de
nikigor <n.gorbadey@gmail.com>
2019-01-08 13:16:09 +0300

    Stable backend version
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class ID_Manager(models.Model):
+    current_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.current_id)
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
+import json
+import os
 
 from django import forms
-
+from .models import ID_Manager
+from .stencil import *
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
     file = forms.FileField()
-
 
 def index(request):
     return render(request, 'core/basic.html')
@@ -18,9 +20,40 @@ def upload(request):
     return render(request, 'core/upload.html')
 
 
-def process(request):  # THAT WORKS
-    if request.method == 'POST':
-        uploaded_image = request.FILES['avatar']
-        fs = FileSystemStorage()
-        fs.save(uploaded_image.name, uploaded_image)
-    return HttpResponse('hello world')
+def process(request):
+    if request.method == 'POST' :
+        cur_id = ID_Manager.objects.get(id=0)
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        MEDIA_DIR = os.path.join(BASE_DIR, "media")
+        FILE_DIR = os.path.join(MEDIA_DIR, str(cur_id.current_id))
+        OUTPUT_DIR = os.path.join(FILE_DIR, "output")
+        # Processing file upload
+        if request.GET.get("param") == 'file':
+            uploaded_image = request.FILES['avatar']
+            fs = FileSystemStorage(FILE_DIR)
+            fs.save('input.jpg', uploaded_image)
+            return HttpResponse(str(cur_id.current_id))
+
+        #Processing colors and start algorythm
+        if request.GET.get("param") == 'colors':
+            
+            #Parse json file
+            body_unicode = request.body.decode('utf-8')
+            body = json.loads(body_unicode)
+            content = body['id']
+            print(body['colors'])
+
+            #Open input file
+            im = Image.open(FILE_DIR + '/input.jpg')
+            os.mkdir(OUTPUT_DIR)
+            
+            #Start algorythm and save output
+            edges, merged_colors, merged_colors_smooth, separate_clrs_pics = find_color_edges(im, target_colors)
+            for i in range(len(edges)):
+                edges[i].save(OUTPUT_DIR + '/' + str(i) + '.jpg', "JPEG")
+            merged_colors_smooth.save(OUTPUT_DIR + '/' + 'result.jpg')
+
+            cur_id.current_id += 1
+            cur_id.save()
+            return HttpResponse('Everything is fine, I will just process it')
+    return HttpResponse('Wrong parameter')
