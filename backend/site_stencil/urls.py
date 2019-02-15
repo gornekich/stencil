335e30208ade3aec6404253a93ad9677c4b435b0
nikigor <n.gorbadey@gmail.com>
2019-02-16 00:05:22 +0300

    Change core url
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

--- backend/site_stencil/urls.py
+++ backend/site_stencil/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('core/', include('core.urls')),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
